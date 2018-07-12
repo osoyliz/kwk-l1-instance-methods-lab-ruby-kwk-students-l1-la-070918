a49a8663c271290b5064e0a4876af3149644032c
osoyliz <lizosoy18@gmail.com>
2018-07-12 23:15:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
 # dog.rb
+class Dog
+  attr_accessor :bark, :sit
+  def initialize (bark, sit)
+    @bark = bark
+    @sit = sit
+  end
+  = User.new("Woof", "The Dod is sitting")
+  puts .bark
+  puts .sit
+end
